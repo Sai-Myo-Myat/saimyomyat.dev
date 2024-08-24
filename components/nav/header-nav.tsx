c0048e233fc7039f5fbb40750d6657bb15b3a19f
Sai-Myo-Myat <saimyomyat511@gmail.com>
2024-08-25 00:51:37 +0700

    style: adjusted some styling for header nav

--- components/nav/header-nav.tsx
+++ components/nav/header-nav.tsx
@@ -4,13 +4,13 @@ import ClickableH3 from "../base/clickable-h3";
 const navLinks: { url: string; text: string }[] = [
   { url: "#profile", text: "Profile" },
   { url: "#projects", text: "Projects" },
-  { url: "#experiences", text: "Experiences" },
+  // { url: "#experiences", text: "Experiences" },
   { url: "#blogs", text: "Blogs" },
 ];
 
 const HeaderNav = () => {
   return (
-    <nav className="flex flex-wrap justify-between py-4 sticky top-0 bg-blue-700">
+    <nav className="flex flex-wrap justify-between py-4 sticky top-0 z-50 bg-background border-b">
       {navLinks.map((link) => {
         return (
           <Link key={link.url} href={link.url}>
