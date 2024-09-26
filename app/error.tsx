b66572241da26c43d1cc71867f52bff68b4946fe
Sai-Myo-Myat <saimyomyat511@gmail.com>
2024-09-26 18:37:53 +0700

    fix: error page's messages

--- app/error.tsx
+++ app/error.tsx
@@ -20,14 +20,14 @@ export default function Error({
   return (
     <div className="min-h-screen max-w-screen-lg mx-auto px-6 sm:px-4">
       <div className="flex flex-col justify-center items-center min-h-screen">
-        <h1 className="text-7xl text-primary">404</h1>
+        <h1 className="text-7xl text-primary">500</h1>
         <div className="flex items-center gap-2">
           <p className="text-lg">Something went wrong...</p>
           <BanIcon size={16} />
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 py-4">
           <GoBack />
-          <Button onClick={reset}>Try Again</Button>
+          <Button onClick={reset}>Reload</Button>
         </div>
       </div>
     </div>
