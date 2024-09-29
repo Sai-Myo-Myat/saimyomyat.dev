286e122d289589954c507feafae25a92842d9448
Sai-Myo-Myat <saimyomyat511@gmail.com>
2024-09-29 23:45:08 +0700

    feat: updated robots and sitemap files

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: `/sitemap`,
+    sitemap: `https://portfolio-seven-kohl-54.vercel.app/sitemap`,
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,9 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: process.env.NEXT_PUBLIC_URL ?? "/",
+      url:
+        process.env.NEXT_PUBLIC_URL ??
+        "https://portfolio-seven-kohl-54.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
