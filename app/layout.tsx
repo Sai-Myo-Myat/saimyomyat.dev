529c300d64956e2ec35c18e9014ab929d22d2384
Sai-Myo-Myat <saimyomyat511@gmail.com>
2024-09-29 20:30:44 +0700

    feat: added some basic og tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ export const metadata: Metadata = {
     telephone: true,
     address: true,
   },
+  openGraph: {
+    title: { template: "%s | Sai Myo Myat", default: "Sai Myo Myat" },
+    description:
+      "I am a full-stack developer with over two years of experiences in developing createive websites and mobile apps",
+  },
 };
 
 export default function RootLayout({
