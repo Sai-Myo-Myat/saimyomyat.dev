195e30a471db76b562f940cb3b9e16c27a3ae674
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-03-14 06:41:48 +0700

    feat: remove layout from 404 page

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -1,5 +1,11 @@
 import ContentNotFound from "@/components/content-not-found";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404",
+};
+
 export default function NotFound() {
   return <ContentNotFound message="Page not found..." />;
 }
