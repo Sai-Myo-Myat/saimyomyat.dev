f9e60e7fa6a844874f629de58cff3ef443aaa316
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-03-14 04:51:49 +0700

    feat: get single blog (api function)

--- api/blogs.ts
+++ api/blogs.ts
@@ -1,5 +1,6 @@
 import { BlogType } from "@/types/blogs";
 import { useQuery } from "@tanstack/react-query";
+import { useQueryFunctionWrapper } from "./base";
 
 const fetchBlogs = async () => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_CMS_BASE}/blogs`);
@@ -9,15 +10,22 @@ const fetchBlogs = async () => {
 
 export function useBlogs() {
   return useQuery({
-    queryKey: ["blogposts"],
+    queryKey: ["blogs"],
     queryFn: fetchBlogs,
   });
 }
 
 const fetchBlog = async (slug: string) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_CMS_BASE}/blogs?filters[slug][$eq]=${slug}&populate=*`
+    `${process.env.NEXT_PUBLIC_CMS_BASE}/blogs?filters[slug][$eq]=${slug}`
   );
   const result = await response.json();
-  return result.data as BlogType;
+  return result.data[0] as BlogType;
 };
+
+export function useBlog(slug: string) {
+  return useQuery({
+    queryKey: ["blog-detail"],
+    queryFn: useQueryFunctionWrapper<string, BlogType>(fetchBlog, slug),
+  });
+}
