dea09a242034c3bf6028e498dfb63446bed822b5
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-03-14 18:32:43 +0700

    feat: added home button

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,6 +1,7 @@
+import ClickableH3 from "@/components/base/clickable-h3";
 import GoBack from "@/components/base/go-back-button";
 import NavLayout from "@/components/nav/nav-layout";
-import { ArrowLeftCircle } from "lucide-react";
+import { ArrowLeftCircle, Home } from "lucide-react";
 import { PropsWithChildren } from "react";
 
 function Layout({ children }: PropsWithChildren) {
@@ -10,6 +11,12 @@ function Layout({ children }: PropsWithChildren) {
         <GoBack>
           <ArrowLeftCircle size={24} />
         </GoBack>
+        <ClickableH3 className="mt-0">
+          <a href="/" className="text-primary flex items-center gap-2">
+            <Home size={16} />
+            Home
+          </a>
+        </ClickableH3>
       </NavLayout>
       {children}
     </>
--- components/nav/nav-layout.tsx
+++ components/nav/nav-layout.tsx
@@ -3,7 +3,9 @@ import React, { PropsWithChildren } from "react";
 const NavLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <nav className="bg-background sticky top-0 z-10">
-      <div className="flex flex-wrap justify-between py-6">{children}</div>
+      <div className="flex flex-wrap items-center justify-between py-6">
+        {children}
+      </div>
     </nav>
   );
 };
