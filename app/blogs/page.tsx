43bee87a0c07461b4500630d348acec69f1ef85c
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-03-14 17:17:11 +0700

    feat: refactor blog list page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,51 +1,12 @@
-"use client";
-
 import Loader from "@/components/Loader";
-import BlogCard from "@/components/blogs/blog-card";
-import ContentNotFound from "@/components/content-not-found";
-import { cn } from "@/lib/utils";
-import { Bree_Serif } from "next/font/google";
-import { useBlogs } from "../../api/blogs";
-
-const bree_serif = Bree_Serif({ weight: "400", subsets: ["latin"] });
-
+import BlogListView from "@/components/blogs/blog-list-view";
 export default function BlogListPage() {
-  const { data: blogs, isLoading, error } = useBlogs();
-
-  if (isLoading) {
-    return (
-      <Loader>
-        <span>{`saimyomyat.blogs`}</span>
-      </Loader>
-    );
-  }
-
-  if (error || !blogs || blogs?.length === 0) {
-    return <ContentNotFound />;
-  }
-
   return (
     <>
       <Loader>
         <span>{`saimyomyat.blogs`}</span>
       </Loader>
-      <div className="flex gap-2">
-        <h1 className={cn(bree_serif.className, "text-primary font-extrabold")}>
-          {`Sai Myo Myat's Blogs`}
-        </h1>
-      </div>
-      <div className="flex flex-col gap-2 py-8">
-        <p>Welcome to my personal blog.</p>
-        <p className="my-1">
-          Here are some of the blog posts that I have written over the years.
-          Feel free to check them out! 🚀
-        </p>
-      </div>
-      <div className="my-4 flex flex-col gap-4">
-        {blogs?.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} href={`/blogs/${blog.slug}`} />
-        ))}
-      </div>
+      <BlogListView />
     </>
   );
 }
---
+++ components/blogs/blog-list-view.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import BlogCard from "@/components/blogs/blog-card";
+import ContentLoading from "@/components/content-loading";
+import ContentNotFound from "@/components/content-not-found";
+import { cn } from "@/lib/utils";
+import { Bree_Serif } from "next/font/google";
+import { useBlogs } from "../../api/blogs";
+
+const bree_serif = Bree_Serif({ weight: "400", subsets: ["latin"] });
+
+export default function BlogListView() {
+  const { data: blogs, isLoading, error } = useBlogs();
+
+  if (isLoading) {
+    return <ContentLoading>Loading...</ContentLoading>;
+  }
+
+  if (error || !blogs || blogs?.length === 0) {
+    return <ContentNotFound />;
+  }
+
+  return (
+    <>
+      <div className="flex gap-2">
+        <h1 className={cn(bree_serif.className, "text-primary font-extrabold")}>
+          {`Sai Myo Myat's Blogs`}
+        </h1>
+      </div>
+      <div className="flex flex-col gap-2 py-8">
+        <p>Welcome to my personal blog.</p>
+        <p className="my-1">
+          Here are some of the blog posts that I have written over the years.
+          Feel free to check them out! 🚀
+        </p>
+      </div>
+      <div className="my-4 flex flex-col gap-4">
+        {blogs?.map((blog) => (
+          <BlogCard key={blog.id} blog={blog} href={`/blogs/${blog.slug}`} />
+        ))}
+      </div>
+    </>
+  );
+}
