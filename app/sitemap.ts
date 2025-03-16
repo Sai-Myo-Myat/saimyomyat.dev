2cc4159b04927e8bba2e90451c70501f28d1da16
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-03-16 16:36:53 +0700

    chore: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/blogs`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/blogs/about-this-site`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/blogs/big-o-notation-part-1`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
